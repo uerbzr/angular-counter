c286b59d0b61fb2c284af07c5ed4a67eaa3df707
uerbzr <nigel.sibbert@gmail.com>
2024-03-08 12:17:24 +0000

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,23 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-counter';
+
+  counter: number = 0;
+
+  increment() {
+    this.counter++;
+  }
+  decrement() {
+    this.counter--;
+  }
+  reset() {
+    this.counter = 0;
+  }
+  bytwo() {
+    this.counter = this.counter * 2;
+  }
 }
